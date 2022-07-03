0c3dda662a08af392b3b46bf111d508e74779d5b
Kristoffer Spencer <whobanil80@linuxmail.org>
2022-07-02 20:34:14 -0700

    Readied player and goblin models

--- src/main/java/com/github/spencerk/models/Goblin.java
+++ src/main/java/com/github/spencerk/models/Goblin.java
@@ -1,4 +1,45 @@
 package com.github.spencerk.models;
 
+import java.util.Random;
+
 public class Goblin {
+    private static final Random random              = new Random();
+    private short               health,
+                                maxHealth;
+    private byte                strength,
+                                level;
+
+    private Goblin(Player player) {
+        byte gobStr         = (byte) (player.getStrength() - 2);
+
+        health              = (byte) (player.getHealth() - 5 - random.nextInt(4));
+        maxHealth           = health;
+        strength            = gobStr > 0 ? gobStr : 1;
+        level               = player.getLevel();
+    }
+
+    public short getHealth() {
+        return health;
+    }
+
+    public byte getStrength() {
+        return strength;
+    }
+
+    public byte getLevel() {
+        return level;
+    }
+
+    public void takeDamage(short dmg) {
+        this.health -= dmg;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Goblin wielding a spear and looking around with wild eyes. Current health: %d/%d",
+                health,
+                maxHealth
+        );
+    }
 }
--- src/main/java/com/github/spencerk/models/Player.java
+++ src/main/java/com/github/spencerk/models/Player.java
@@ -1,4 +1,84 @@
 package com.github.spencerk.models;
 
+import java.util.Random;
+
 public class Player {
+    private static Player       player;
+    private static final Random random              = new Random();
+    private short               health,
+                                maxHealth;
+    private byte                strength,
+                                level;
+    private String              name;
+    private int                 currentExp,
+                                expNeededToLevel;
+
+    private Player() {
+        name                = "Tivian";
+        health              = 10;
+        maxHealth           = 10;
+        strength            = 2;
+        level               = 1;
+        currentExp          = 0;
+        expNeededToLevel    = 20;
+    }
+
+    public Player getInstnace() {
+        if(player == null) player = new Player();
+        return player;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public short getHealth() {
+        return health;
+    }
+
+    public byte getStrength() {
+        return strength;
+    }
+
+    public byte getLevel() {
+        return level;
+    }
+
+    public int getCurrentExp() {
+        return currentExp;
+    }
+
+    public int getExpNeededToLevel() {
+        return expNeededToLevel;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void takeDamage(short dmg) {
+        this.health -= dmg;
+    }
+
+    public void addExp(int exp) {
+        currentExp += exp;
+
+        if(currentExp < expNeededToLevel) {
+            currentExp -= expNeededToLevel;
+            expNeededToLevel *= .25;
+            levelUp();
+        }
+    }
+
+    private void levelUp() {
+        this.maxHealth += 2;
+        //Player heals on level up
+        this.health = maxHealth;
+        this.strength += random.nextInt(2) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is a level %d warrior. Current health: %d/%d");
+    }
 }
